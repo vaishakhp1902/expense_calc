b765cf966be6c8dc95088ae1024b5a250e83eafa
Vaishakh <vaishakhp1998@gmail.com>
2021-10-23 23:11:03 +0530

    Updates

---
+++ precache-manifest.c4db8127a2e5a97d9f7f05e97c74b36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdcbd6ac3d19ef04f6208633bb8324e",
+    "url": "/expense_calc/index.html"
+  },
+  {
+    "revision": "5ca26cff406ec15010e1",
+    "url": "/expense_calc/static/css/main.7fa2af12.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expense_calc/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense_calc/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca26cff406ec15010e1",
+    "url": "/expense_calc/static/js/main.fca6a4aa.chunk.js"
+  },
+  {
+    "revision": "505656187befe9461c09",
+    "url": "/expense_calc/static/js/runtime-main.230f4d3b.js"
+  }
+]);
